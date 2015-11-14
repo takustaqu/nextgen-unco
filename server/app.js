b3b250bb99e640b251ca6acaa1b6b86fcb50df2c
Yuya Matsuo <niu@niusounds.com>
2015-11-15 05:58:16 +0900

    Cross originヘッダー

--- server/app.js
+++ server/app.js
@@ -18,6 +18,11 @@ var accessLogStream = require('fs').createWriteStream(__dirname + '/access.log',
 app.use(morgan('combined', {
   stream: accessLogStream
 }));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(router);
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/../ui_designing/build'));
